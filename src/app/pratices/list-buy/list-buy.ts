67fc39d2f9f1dfacd7f43937280a835fb3bd545c
Anderton Santos <anderton9814@outlook.com>
2025-09-12 09:45:20 -0300

    new commit

--- src/app/pratices/list-buy/list-buy.ts
+++ src/app/pratices/list-buy/list-buy.ts
@@ -1,4 +1,4 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, JsonPipe } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms'
 
@@ -12,6 +12,8 @@ export class ListBuy {
 
   items = signal<{ product: string; price: string; category: string }[]>([])
 
+  indexEdit = signal<null | number>(null)
+
   listCategory = signal<string[]>([
     "Alimentos",
     "Limpeza",
@@ -23,21 +25,49 @@ export class ListBuy {
 
   submitForm(form: NgForm) {
     if (form.valid) {
-      this.items.update((prev) => [
-        ...prev, {
-          product: form.value.inputProduct,
-          price: form.value.inputPrice,
-          category: form.value.selectCategory
-        }
-      ])
+
+
+      if (this.indexEdit() != null) {
+        this.items.update(prev => {
+          const copy = [...prev]
+          copy[this.indexEdit()!] = {
+            product: form.value.inputProduct,
+            price: form.value.inputPrice,
+            category: form.value.selectCategory
+          }
+          return copy
+        })
+
+        this.indexEdit.set(null)
+      } else {
+        this.items.update((prev) => [
+          ...prev, {
+            product: form.value.inputProduct,
+            price: form.value.inputPrice,
+            category: form.value.selectCategory
+          }
+        ])
+      }
 
       form.reset()
     }
 
-
-
-
-    console.log(form.value)
   }
+
+  removeItem(item: string) {
+    this.items.update(prev => prev.filter(p => p.product !== item))
+  }
+
+  handleEdit(index: number, form: NgForm) {
+    this.indexEdit.set(index)
+
+    const item = this.items()[index]
+    form.setValue({
+      inputProduct: item.product,
+      inputPrice: item.price,
+      selectCategory: item.category
+    })
+  }
+
 
 }
